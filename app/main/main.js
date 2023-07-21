6aed22835939863c0ece64ff4701a598c7630220
Ping <64058804+real-ping@users.noreply.github.com>
2023-07-21 22:09:09 +1000

    finished lol

--- app/main/main.js
+++ app/main/main.js
@@ -7,68 +7,118 @@ class="bi bi-folder2-open" viewBox="0 0 16 16">
 
 
 (async () => {
+    const folderUpBtn = document.getElementById("folderUpBtn");
+    const directoryInfo = document.getElementById('directoryInfo')
+    const pathInputEl = document.getElementById('pathInputEl')
+
+
     let currentDir = (await window.api.invoke("getCurrentDir")).toString();
     console.log(`Starting Dir: ${currentDir}`)
     pathInputEl.value = currentDir;
 
-    const folderUpBtn = document.getElementById("folderUpBtn");
-    const directoryInfo = document.getElementById('directoryInfo')
     updateDirectoryButtons()
 
-    function updateDirectoryButtons() {
-        
-        if (`${currentDir}`.split("/").length == 1) {
-            currentDir = currentDir+="/"
-            folderUpBtn.classList.add('disabled')
+    pathInputEl.addEventListener("keydown", (async (e) => {
+        console.log(e);
+        if (e.key == "Enter") {
+            const pathExists = await window.api.invoke("doesPathExist", pathInputEl.value)
+            if (pathExists) {
+                pathInputEl.blur();
+                currentDir = pathInputEl.value;
+                updateCurrentDirectoryFiles()
+                updateDirectoryButtons()
+            } else {
+                // pathInputEl.blur();
+                pathInputEl.value = currentDir;
+            }
         }
+    }))
+
+    function updateDirectoryButtons() {
+
         pathInputEl.value = currentDir;
 
-        if(this.currentFilesAndFolders){
+        if (`${currentDir}`.length <= 3) {
+            currentDir = currentDir += "/"
+            folderUpBtn.classList.add('disabled')
+        } else {
+            folderUpBtn.classList.remove('disabled')
+        }
+        if (this.currentFilesAndFolders) {
             directoryInfo.innerText = `${currentFilesAndFolders.filter(x => !x.isFolder).length} files | ${currentFilesAndFolders.filter(x => x.isFolder).length} folders`
         }
     }
 
-    async function updateCurrentDirectoryFiles(){
+    async function updateCurrentDirectoryFiles() {
         const currentFilesAndFolders = await window.api.invoke("getDirFilesAndFolders", currentDir);
+        // if(currentFilesAndFolders)
+        console.log(currentFilesAndFolders)
 
         let html = `<div class="directoryInfo" id="directoryInfo">${currentFilesAndFolders.filter(x => !x.isFolder).length} files | ${currentFilesAndFolders.filter(x => x.isFolder).length} folders</div>`;
         explorer.innerHTML = html;
-    
-        currentFilesAndFolders.forEach((file) => {
+
+        iterateFiles(0)
+
+        function iterateFiles(i) {
+            if (i >= currentFilesAndFolders.length) return;
+            const file = currentFilesAndFolders[i];
             console.log(file);
-    
+
             const fileEl = document.createElement('div');
             fileEl.classList.add("file");
-    
+
             const iconEl = document.createElement('div');
             iconEl.classList.add("icon");
             fileEl.append(iconEl)
-    
+
             const iconIMG = document.createElement('img');
-            iconIMG.src = file.icon;
+            iconIMG.width = "32"
+            iconIMG.height = "32"
+            iconIMG.src = file.isFolder ? "./../folder.png" : file.icon ? file.icon : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAxUlEQVRYhWNgGGDAiE2worToPiMTkwI5Bv7/9+/Bj9//AidMmHCBbAdUlpf8b2xuI8d+hvraKgYODvYP7z9+dSTGEUxk2UIAJCalCgjyc+8vKCgwGBAHSEhKEe0ImjiAFEfQzAHEOoKmDiDGESzUtlBAQIChvrYKqxQbG/N+BgYGQZo6oLC4DKdcfW2VALoYzaOAEBh1wKgDRh0w6oBRB4w6YNQBow4YdcCAOwBro/T/v38P6muryOqc4gP///17QG0zKQYAgws6WA41L8EAAAAASUVORK5CYII=";
             iconEl.append(iconIMG);
-    
+
             const fileNameEl = document.createElement("div");
             fileNameEl.classList.add("filename");
             fileNameEl.innerText = file.filename;
             fileEl.append(fileNameEl);
 
             fileNameEl.onclick = (() => {
-                window.api.send("fil")
+                if (file.isFolder) {
+                    currentDir += (currentDir.endsWith("/") ? "" : "/") + file.filename
+                    console.log("loading dir:", currentDir)
+                    updateDirectoryButtons()
+                    updateCurrentDirectoryFiles()
+                } else {
+                    window.api.invoke("openFile", currentDir + "/" + file.filename)
+                    updateDirectoryButtons()
+                    updateCurrentDirectoryFiles()
+                }
             })
-    
+
             const revearlInFileExplorerEl = document.createElement("div");
             revearlInFileExplorerEl.classList.add("show");
             revearlInFileExplorerEl.title = `Show more options`;
             revearlInFileExplorerEl.innerHTML = `...`;
             fileEl.append(revearlInFileExplorerEl);
-    
+
             explorer.append(fileEl)
-    
-        })
+            i++;
+            iterateFiles(i);
+        }
+
     }
 
-  
+
+    cmd.onclick = (async () => {
+        const success = await window.api.invoke("startCMD", currentDir);
+
+    })
+
+    closeEl.onclick = (async () => {
+        const success = await window.api.invoke("close", currentDir);
+
+    })
+
     folderUpBtn.onclick = (async () => {
         if (folderUpBtn.classList.contains("disabled")) return
         currentDir = `${currentDir}`.split("/");
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const Electron = require('electron');
 const fs = require('fs')
+const open = require("open");
+const c = require('child_process');
+const os = require('os');
 
 Electron.app.whenReady().then(() => {
 
@@ -28,23 +31,51 @@ Electron.app.whenReady().then(() => {
         return NI.toDataURL()
     })
     Electron.ipcMain.handle('getCurrentDir', async (e, path) => {
-        return "C:/Users/maxta/Desktop/.temp/icon"
+        return os.homedir().replaceAll("\\", "/")
     })
-
+    Electron.ipcMain.handle('startCMD', async (e, path) => {
+        c.exec(`start`, {
+            cwd: path
+        })
+    })
+    Electron.ipcMain.handle('close', async (e) => {
+        Electron.app.quit()
+    })
+    Electron.ipcMain.handle('openFile', async (e, path) => {
+        console.log(`opening:`, path)
+        open(path)
+        return true;
+    })
+    Electron.ipcMain.handle('doesPathExist', async (e, path) => {
+        return fs.existsSync(path) && fs.lstatSync(path).isDirectory();
+    })
     Electron.ipcMain.handle('getDirFilesAndFolders', async (e, dir) => {
         let data = fs.readdirSync(dir, 'utf-8');
         await new Promise((res) => {
-            data.forEach(async (x, i) => {
-                let icon = (await Electron.app.getFileIcon(path.join(dir, x))).toDataURL();
-                data[i] = {
-                    filename: x,
-                    isFolder: fs.lstatSync(path.join(dir, x)).isDirectory(),
-                    icon
-                };
-                if (i == data.length - 1) res();
-            })
+            iterateFiles(0)
+            async function iterateFiles(i) {
+                let isFolder, icon;
+                let x = data[i]
+                icon = (await Electron.app.getFileIcon(path.join(dir, x))).toDataURL();
+                fs.lstat(path.join(dir, x), ((err, stats) => {
+                    if (err) {
+                        // console.log(`file error at:`, x, err.code)
+                        isFolder = false;
+                    } else {
+                        isFolder = stats.isDirectory()
+                    }
+                    data[i] = {
+                        filename: x,
+                        isFolder,
+                        icon
+                    };
+                    if (i == data.length - 1) res();
+                    else iterateFiles(i + 1)
+                }))
+            }
+
         });
-        console.log(data)
+        // console.log(data)
         return data;
     })
 
